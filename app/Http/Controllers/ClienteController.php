d1d149adb80555e8daea33097b39c347fc8ee5f6
bernardoBND <bernardonegriniborba@gmail.com>
2024-10-09 21:15:50 -0300

    second commit

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,16 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClienteController extends Controller
 {
     public function listar()
     {
-        $array = [
-            "nome" => "Nome teste"
-        ];
+        
+    }
+    public function salvar(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|email|max:200',
+        ]);
 
-        return response()->json($array, 200);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $customer = Cliente::create($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Customer created successfully',
+            'data' => $customer
+        ], 201);
+    }
+    public function listaPeloId(int $id)
+    {
+
+    }
+    public function editar(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|email|max:200',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $customer = Cliente::findOrFail($id);
+        $customer->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Customer created successfully',
+            'data' => $customer
+        ], 201);
+    }
+    public function deletar(int $id)
+    {
+
     }
 }
